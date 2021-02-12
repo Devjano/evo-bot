add2b9407c433967e2d69a6f4924efdbd8d2d944
jano <77622056+Devjano@users.noreply.github.com>
2021-02-11 23:40:10 -0800

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,17 +1,22 @@
-const { LOCALE } = require("../util/EvobotUtil");
-const i18n = require("i18n");
-
-i18n.setLocale(LOCALE);
+ const { Client, Collection, MessageEmbed } = require(`discord.js`);
+const {
+  PREFIX,
+  approveemoji,
+  denyemoji
+} = require(`../config.json`);
 
 module.exports = {
-  name: "invite",
-  description: i18n.__('invite.description'),
-  execute(message) {
-    return message.member
-      .send(
-        `https://discord.com/oauth2/authorize?client_id=${message.client.user.id}&permissions=70282305&scope=bot
-    `
-      )
-      .catch(console.error);
+  name: `invite`,
+  description: `Gives you an invite`,
+  aliases: ["add"],
+  cooldown: 4,
+  edesc: "Type this command to get an invite link for the Bot, thanks for every Invite",
+  execute(message, args, client) {
+    //react with approve emoji
+    message.react("769665713124016128");
+    //send the invite embed
+    message.reply(new MessageEmbed().setColor("BLACK").setTitle("**link bot**").setDescription("https://discord.com/api/oauth2/authorize?client_id=780578520870158337&permissions=8&scope=bot")
+    .setFooter("🥺Invite Bot🥺", "https://phoneky.co.uk/thumbs/screensavers/down/music/music_me3s1nz7.gif"));
+
   }
-};
+}
