43d13221d0e2cfc851f755efd8787c9d6e194009
jano <77622056+Devjano@users.noreply.github.com>
2021-02-11 23:38:42 -0800

    Update shuffle.js

--- commands/shuffle.js
+++ commands/shuffle.js
@@ -5,6 +5,7 @@ i18n.setLocale(LOCALE);
 
 module.exports = {
   name: "shuffle",
+  aliases: ["sh"],
   description: i18n.__('shuffle.description'),
   execute(message) {
     const queue = message.client.queue.get(message.guild.id);
