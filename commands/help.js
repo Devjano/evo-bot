db27e94347f4eddb2089e3d76bd148933fe58110
JANO <77622056+Devjano@users.noreply.github.com>
2021-10-24 15:21:42 -0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,40 +1,27 @@
- const { MessageEmbed } = require("discord.js");
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
   name: "help",
   aliases: ["h"],
-  cooldown: 8,
+  cooldown: 5,
   description: "**all commands**",
   execute(message) {
     let commands = message.client.commands.array();
 
     let helpEmbed = new MessageEmbed()
-    .setTitle(`**Hyper Music  Commands**`)
-    .setThumbnail("https://media.tenor.com/images/3145445a2c5bde03ac708c2c309fdbca/tenor.gif")
-    .setAuthor("Prefix [ $ ]","")
+    .setTitle(`**Ability  Commands**`)
+
     .setDescription(` 
-\`Help Commands\`
+> **Music Command**
+> \` %paly\` • \`%stop\` • \`%skip\` • \`%skipto\` • \`%volume \`
+> \` %queue\` • \`%loop\` • \`%pause\` • \`%nowplaying \`
+> \` %shuffle\` • \`%uptime\` • \`%search\` • \`%remove \`
+> \` %radio\` • \`%filter\` • \`%lyrics\` • \`%resume\`
+> **Info Command**
+> \` %bot\` • \`%support\` • \`%invite\` • \`%help\` • \`%ping\`
 
-**Music Command**
-
-\` $paly , $stop , $skip , $skipto , $volume \`
-
-\` $queue , $loop , $pause , $nowplaying \`
-
-\` $shuffle , $uptime , $search , $remove \`
-
-\` $clip , $clips , $lyrics , $move , $playlist \`
-
-**Info Command**
-
-\` $bot , $support , $invite , $help , $ping \`
-
-
-
-     
-**[ invite ](https://discord.com/api/oauth2/authorize?client_id=780578520870158337&permissions=8&scope=bot)** , **[ support ](https://discord.gg/3wykSpqjZq)**      
-
-**[ vote ](https://top.gg/bot/780578520870158337)**                                                                
+> **[ \`invite\` ](https://discord.com/api/oauth2/authorize?client_id=867208331659706379&permissions=0&scope=bot)** 
+> **[ \`support\` ](https://discord.gg/7sEtecbAM6)**                                                              
  `)
     
     helpEmbed.setTimestamp();
